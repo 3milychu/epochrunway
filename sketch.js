d3cdc8f333448e71d3e4eefc5f59dba9309babce
3milychu <3milychu@gmail.com>
2018-03-05 18:23:31 -0500

    push

--- sketch.js
+++ sketch.js
@@ -11,12 +11,14 @@ function setup(){
 
 	getInfo();
 
+	updateStart();
+
 	};
 
 
 function getInfo(){
 
-d3.csv("https://github.com/3milychu/majorstudio/raw/master/project2/analysis/costume_reclass.csv", function(data) {
+d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/data/clothes/costume_reclass.csv", function(data) {
 	  		data.forEach(function(d) {
 	   			d.objectBegin = +d.objectBegin;
 	   			 });
@@ -49,7 +51,7 @@ d3.csv("https://github.com/3milychu/majorstudio/raw/master/project2/analysis/cos
 
 function updateStart() {
 
-	d3.csv("https://github.com/3milychu/majorstudio/raw/master/project2/analysis/costume_reclass.csv", function(data) {
+	d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/data/clothes/costume_reclass.csv", function(data) {
 	  		data.forEach(function(d) {
 	   			d.objectBegin = +d.objectBegin;
 	   			 });
@@ -69,5 +71,81 @@ function updateStart() {
 			  footwear = data.filter(function(d) { 
 			    	return d.bodyclass1 == "feet"});
 
+
+	var randomFootwear = Math.floor((Math.random() * footwear.length) + 0);
+
+	for (i=0;i<9;i++){
+		d3.select(".wheelbox"+[i]).selectAll("img").remove();
+		var featuredFootwear = d3.select(".wheelbox"+[i]).selectAll("#wheelbox"+[i])
+			.data(footwear.filter(function (d, i) { return i === randomFootwear;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .style("background-position","center")
+	        .style("background-size","30%")
+	        .attr("src",function(d) {return "https://raw.githubusercontent.com/3milychu/majorstudio/master/png/" + d.img+".png";})
+	        .exit();
+
+	};
+
+	d3.select(".featured-feet").selectAll("img").remove();
+		var featuredFootwear = d3.select(".featured-feet").selectAll("#featured-feet")
+			.data(footwear.filter(function (d, i) { return i === randomFootwear;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .style("background-position","center")
+	        .style("background-size","30%")
+	        .attr("src",function(d) {return "https://raw.githubusercontent.com/3milychu/majorstudio/master/png/" + d.img+".png";})
+	        .exit();
+
+	var randomEnsemble = Math.floor((Math.random() * fullbody.length) + 0);
+
+	for (i=0;i<9;i++){
+		d3.select(".top-spin"+[i]).selectAll("img").remove();
+		var featuredEnsemble= d3.select(".top-spin"+[i]).selectAll("#top-spin"+[i])
+			.data(fullbody.filter(function (d, i) { return i === randomEnsemble;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .style("background-position","center")
+	        .style("background-size","30%")
+	        .attr("src",function(d) {return "https://raw.githubusercontent.com/3milychu/majorstudio/master/png/" + d.img+".png";})
+	        .exit();
+
+	};
+	
+	d3.select(".featured-top").selectAll("img").remove();
+		var featuredEnsemble = d3.select(".featured-top").selectAll("#featured-top")
+			.data(fullbody.filter(function (d, i) { return i === randomEnsemble;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .style("background-position","center")
+	        .style("background-size","30%")
+	        .attr("src",function(d) {return "https://raw.githubusercontent.com/3milychu/majorstudio/master/png/" + d.img+".png";})
+	        .exit();
+
+	var randomHeadwear = Math.floor((Math.random() * head.length) + 0);
+
+	d3.select(".featured-head").selectAll("img").remove();
+		var featuredEnsemble = d3.select(".featured-head").selectAll("#featured-head")
+			.data(head.filter(function (d, i) { return i === randomHeadwear;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .style("background-position","center")
+	        .style("background-size","30%")
+	        .attr("src",function(d) {return "https://raw.githubusercontent.com/3milychu/majorstudio/master/png/" + d.img+".png";})
+	        .exit();
+
+	        // end getcsv
 			});
+
+// end updateStart
 };
